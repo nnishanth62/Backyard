e257b54152b73a149a5f4f02f8ce569fa5832740
alecasas <ac7262@nyu.edu>
2021-10-12 11:21:51 -0400

    delete post and order post by date

--- backyard/blog/urls.py
+++ backyard/blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-#from . import views
-from .views import HomeView, PostDetailView, MakePostView, UpdatePostView
+# from . import views
+from .views import HomeView, PostDetailView, MakePostView, UpdatePostView, DeletePostView
 
 urlpatterns = [
-    #path('', views.home, name = "home"),
-    path('', HomeView.as_view(), name = "home"),
-    path('post/<int:pk>', PostDetailView.as_view(), name = "post_detail"),
-    path('make_post/', MakePostView.as_view(), name = "make_post"),
-    path('post/edit/<int:pk>', UpdatePostView.as_view(), name = "update_post"),
+    # path('', views.home, name = "home"),
+    path('', HomeView.as_view(), name="home"),
+    path('post/<int:pk>', PostDetailView.as_view(), name="post_detail"),
+    path('make_post/', MakePostView.as_view(), name="make_post"),
+    path('post/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
+    path('post/<int:pk>/remove', DeletePostView.as_view(), name="delete_post"),
+
 ]
--- backyard/blog/views.py
+++ backyard/blog/views.py
@@ -1,30 +1,43 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 '''
 def home(request):
     return render(request, 'home.html', {})
 '''
+
+
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-post_date']
+    # ordering = ['-id']  # temporary, most recent post appears first
+
 
 class PostDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
 
+
 class MakePostView(CreateView):
     model = Post
     template_name = "make_post.html"
-    form_class = PostForm #from forms.py
-    #fields = "__all__"
-    #fields = ("title", "body")
+    form_class = PostForm  # from forms.py
+    # fields = "__all__"
+    # fields = ("title", "body")
 
-class UpdatePostView(UpdateView): 
-    model = Post 
+
+class UpdatePostView(UpdateView):
+    model = Post
     form_class = EditForm
     template_name = "update_post.html"
-    #fields = ['title', 'title_tag','body'] #cant have this line and the form_class line
+    # fields = ['title', 'title_tag','body'] #cant have this line and the form_class line
 
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = "delete_post.html"
+    success_url = reverse_lazy('home')
