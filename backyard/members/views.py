b1b11f8123c141f75e47d3c5ebdd19eb82ef44a1
Moustadirane Taenchiki <moustadiranetaenchiki@10-21-89-210.dynapool.wireless.nyu.edu>
2021-11-10 02:23:47 -0500

    enable editing profile page, viewing profile page, editing  settings page, working on creating profile page if non-existent

--- backyard/members/views.py
+++ backyard/members/views.py
@@ -18,6 +18,8 @@ class ShowProfilePageView(DetailView):
         context["page_user"] = page_user
         return context
 
+
+
 class EditProfilePageView(generic.UpdateView):
     model = Profile
     template_name = 'registration/edit_profile_page.html'
