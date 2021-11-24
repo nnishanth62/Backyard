2593f0dee917ac7d3f2d17160ccde24e665c072a
Karennarodriguez <kr2266@nyu.edu>
2021-11-24 15:48:40 -0500

    minor updates

---
+++ main/blog/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import datetime, date
+# from ckeditor_uploader.fields import RichTextUploadingField
+from ckeditor.fields import RichTextField
+from django.db import models
+from location_field.models.plain import PlainLocationField
+
+global LENGTH
+LENGTH = 255 
+class Category(models.Model):
+    name = models.CharField(max_length=LENGTH)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        # return reverse("post_detail", args=(str(self.id)))
+        return reverse("home")  # returns to homepage
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+    website_url = models.CharField(max_length=LENGTH, null=True, blank=True)
+    twitter_url = models.CharField(max_length=LENGTH, null=True, blank=True)
+    instagram_url = models.CharField(max_length=LENGTH, null=True, blank=True)
+    tiktok_url = models.CharField(max_length=LENGTH, null=True, blank=True)
+    facebook_url = models.CharField(max_length=LENGTH, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
+    def get_absolute_url(self):
+        return reverse("home")  # returns to homepage
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=LENGTH)
+    header_image = models.ImageField(null=True, blank=True, upload_to="images/")
+    # title_tag = models.CharField(max_length=255)
+    # location = models.CharField(max_length=255)
+    #location = PlainLocationField(based_fields=['city'], zoom=7)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+    post_date = models.DateField(auto_now_add=True)
+    snippet = models.CharField(max_length=LENGTH)
+    category = models.CharField(max_length=LENGTH, default="event")
+    likes = models.ManyToManyField(User, related_name='blog_post')
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
+
+    def get_absolute_url(self):
+        # return reverse("post_detail", args=(str(self.id)))
+        return reverse("home")  # returns to homepage
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    #name = models.CharField(max_length=LENGTH)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title)
