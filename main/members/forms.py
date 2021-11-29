693cf6ec1fc59d04b7b74f42682f9a11a8215e82
Karennarodriguez <kr2266@nyu.edu>
2021-11-28 20:00:44 -0500

    some more updates

--- main/members/forms.py
+++ main/members/forms.py
@@ -45,12 +45,12 @@ class EditProfileForm(UserChangeForm):
     last_login = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     # is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs = {'class': 'form-check'}))
     # is_staff = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs = {'class': 'form-check'}))
-    is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    # is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
     date_joined = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'last_login', 'is_active', 'date_joined')
+        fields = ('username', 'first_name', 'last_name', 'email', 'last_login', 'date_joined')
 
 
 class PasswordChangingForm(PasswordChangeForm):
